735620d330eb98a0fd7bdfb03b162631535c3b80
manny-reynoso <manny.d.reynoso@gmail.com>
2025-05-19 12:01:23 -0500

    added
     dot notation
    
    bracket notation

--- review.js
+++ review.js
@@ -1,9 +1,20 @@
 // overall review, more like notes 
 
+
+// object literal
+
 const myObject ={
     property: 'Value!',
     otherProperty: 77,
     "obnoxious property": function (){
-        // do stuff!
+        //do stuff!
     }
 }
+
+// dot notation
+
+myObject.property; //'Value!''
+
+// bracket notation
+
+myObject["obnoxious property"]; // [Function]
